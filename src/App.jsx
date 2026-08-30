5da2f0651e6f67306425559559844e38c7263038
Muhammad Tahir <2024cs704@student.uet.edu.pk>
2026-08-31 04:28:37 +0500

    feat: switch app to Awwwards portfolio experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import Navigation from './components/Navigation';
-import { Hero, AboutMe, Experience, Certifications, Skills, Projects, Contact, Footer } from './sections';
-import './App.css';
+import AwwwardsPortfolio from './components/AwwwardsPortfolio';
 
 function App() {
-  return (
-    <main>
-      <Navigation />
-      <Hero />
-      <AboutMe />
-      <Experience />
-      <Skills />
-      <Certifications />
-      <Projects />
-      <Contact />
-      <Footer />
-    </main>
-  );
+  return <AwwwardsPortfolio />;
 }
 
 export default App;
